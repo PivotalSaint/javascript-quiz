247a98b01fb9f7a899265ed0541e8d9558a66eb8
PivotalSaint <licensedmt@att.net>
2022-08-09 11:31:49 -0500

    gave a header and title to header with color background and border along with display of body flex

---
+++ README.md
@@ -0,0 +1,7 @@
+## JavaBean PopQuiz
+## Author: Jesse Van Winkle
+
+This activity is a timed activity in which the user interacting with the web page, will answer a series of questions over javascript functioning. Depending on the answers given to the page, the user will generate a score at the end. 
+
+## Screenshot of Project
+<>
---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+
+
+// need to add button that prompts or starts code with a timer
+
+
+
+
+// THEN a timer starts and I am presented with a question
+// timer needs to be set function with total time and a variable to subtract that time
+// Add timer function here
+
+
+
+
+// WHEN I answer a question
+// Need to add function that logs data and keeps the data stored by the window
+// this variable will effect timer as well if not true and is false
+
+
+
+
+
+// THEN I am presented with another question
+// so variable has to loop with continuous timer and function for questions has to continue
+
+
+
+
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// add function to subtract time in milliseconds from overall time left
+
+
+
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
