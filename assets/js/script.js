ded892b1cf7c2fb1d5ba7544615b99644fcf0526
PivotalSaint <licensedmt@att.net>
2022-08-10 16:20:56 -0500

    idk anymore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,82 @@
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
-var startQuiz=document.querySelector("#questionBody");
-var quizTimer= document.querySelector("#quizTimer");
-var answers= document.querySelectorAll("#button");
-var questionList=0;
-var displayQuizScore=0;
-
-var questions = [
-    // question 1
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    // question 2
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    //question 3
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    //question 4
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    //question 5
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    //question 6
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    //question 7
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    //question 8
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    //question 9
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-    //question 10
-    {
-        question:"",
-        choices:"",
-        answers:""
-    },
-];
-
-// need to add button that prompts or starts code with a timer
-// THEN a timer starts and I am presented with a question
-// timer needs to be set function with total time and a variable to subtract that time
-// Add timer function here
+questionList=0;
+displayQuizScore=0;
 var timeLeft = 80;
 var noTime=0;
 var timeOff= 10;
+var questionList
+var quizTimer= document.querySelector(".quizTimer").textContent="The quiz will end in"+timeLeft+"seconds remaining";
 
-quizTimer.addEventListener("click", startQuiz);
 function quizTimer () {
-    var noTime = setInterval( function(){
+    window.prompt("would you like to try this madness?")
+    var displayQuizScore = setInterval( function(){
         console.log(quizTimer)
         if (timeLeft > 1) {
-            timeLeft.textContent = quizTimer + 'seconds remaining';
+            timeLeft.textContent = quizTimer + "seconds remaining";
             timeLeft--;
         }
 
         if (timeLeft ===1) {
-            timeLeft.textContent= timeLeft + 'seconds remaining';
+            timeLeft.textContent= timeLeft + "seconds remaining";
             timeLeft--;
         }
         else {
-            timeLeft.textContent = '';
-            clearInterval(quizTimer);
+            timeLeft.textContent = noTime;
+            clearInterval(displayQuizScore);
             displayQuizScore();
         }
-    var startQuiz=setInterval(quizTimer, 1000);
 
-    });
+    }, 1000);
 };
-function generateQuestions(){
+function questionList(text, choices, answer) {
+    this.text=text;
+    this.choices=choices;
+    this.answer=answer;
+
+    correctAnswer= function(choices){
+        return choices === this.answer;
+    }
+
+var questions= [
     
-
+    // question 1
+    {
+        text:"What is a good way to trace your work in the browser?",
+        choices:"console.log,javaBean,bootCamp, no idea",
+        answer:"console.log"
+    },
+    // question 2
+    {
+        text:"Doing _____ is a good way of storing important coding for reffering to later.",
+        choices:"pull-request, commits, merge-request, no idea",
+        answer:"commits"
+    },
+    //question 3
+    {
+        text:"This scripting helps with giving the user an interacting experience.",
+        choices:"style.css, javaBean, script.js, no idea",
+        answer:"script.js"
+    },
+    //question 4
+    {
+        text:"git ______ -b allows a user to create another branch to work in without effecting the main coding?",
+        choices:"push, pull, checkout, no idea",
+        answer:"checkout"
+    },
+    //question 5
+    {
+        text:"_____ refers to which it came from?",
+        choices:"who, what, no idea, this",
+        answer:"this"
+    },
+];
 }
-
-// WHEN I answer a question
-// Need to add function that logs data and keeps the data stored by the window
-// this variable will effect timer as well if not true and is false
-
-
-
-// THEN I am presented with another question
-// so variable has to loop with continuous timer and function for questions has to continue
-
-
-
-
-
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// add function to subtract time in milliseconds from overall time left
-
-
-
-
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
+window.addEventListener("click", startQuiz);
+function displayQuizScore(){
+    if (startQuiz(quizTimer <=0)
+}
+// need to add button that prompts or starts code with a timer
+// THEN a timer starts and I am presented with a question
+// timer needs to be set function with total time and a variable to subtract that time
+// Add timer function here
